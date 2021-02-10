f5aa305fcbd8d1533a23e526082514a119637a62
Janira F. Vidal <78372643+janirafvidal@users.noreply.github.com>
2021-02-10 16:37:48 +0100

    Add files via upload

--- Criba.java
+++ Criba.java
@@ -1,64 +1,68 @@
-package tarea4;
+package entornos;
 
 import java.util.Scanner;
 
 public class Criba {
-	// Generar n√∫meros primos de 1 a max
+
 	public static int[] generarPrimos(int max) {
-		int i, j;
+		
 		if (max >= 2) {
-			// Declaraciones
-			int dim = max + 1; // Tama√±o del array
-			boolean[] esPrimo = new boolean[dim];
-			// Inicializar el array
-			for (i = 0; i < dim; i++)
+
+			int tamaÒoArray = max + 1;
+			boolean[] esPrimo = new boolean[tamaÒoArray];
+
+			for (int i = 0; i < tamaÒoArray; i++)
 				esPrimo[i] = true;
-			// Eliminar el 0 y el 1, que no son primos
 			esPrimo[0] = esPrimo[1] = false;
-			// Criba
-			for (i = 2; i < Math.sqrt(dim) + 1; i++) {
+
+			for (int i = 2; i < Math.sqrt(tamaÒoArray) + 1; i++) {
 				if (esPrimo[i]) {
-					// Eliminar los m√∫ltiplos de i
-					for (j = 2 * i; j < dim; j += i)
+					for (int j = 2 * i; j < tamaÒoArray; j += i)
 						esPrimo[j] = false;
 				}
 			}
-			// ¬øCu√°ntosprimoshay?
-			int cuenta = 0;
-			for (i = 0; i < dim; i++) {
+
+			int cuantosPrimos = 0;
+			for (int i = 0; i < tamaÒoArray; i++) {
 				if (esPrimo[i])
-					cuenta++;
+					cuantosPrimos++;
 			}
-			// Rellenar el vector de n√∫meros primos
-			int[] primos = new int[cuenta];
-			for (i = 0, j = 0; i < dim; i++) {
+
+			int[] primos = new int[cuantosPrimos];
+			for (int i = 0, j = 0; i < tamaÒoArray; i++) {
 				if (esPrimo[i])
 					primos[j++] = i;
 			}
 			return primos;
-		} else { // max < 2
+		} else {
 			return new int[0];
-			// vector vac√≠o
 		}
 	}
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("Introduce el n√∫mero para la criba de Erast√≥tenes: ");
+		System.out.println("Introduce el n˙mero para la criba de ErastÛtenes: ");
+		
 		int dato = teclado.nextInt();
 		int vector[] = new int[dato];
-		System.out.println("/nVector inicial hasta: " + dato);
+		
+		teclado.close();
+		
+		
+		System.out.println("\nVector inicial hasta: " + dato);
 		for (int i = 0; i < vector.length; i++) {
 			if (i % 10 == 0)
 				System.out.println();
-			System.out.println(i + 1 + "/t");
+			System.out.println(i + 1 + "\t");
 		}
+		
 		vector = generarPrimos(dato);
-		System.out.println("/nVector de primos hasta: " + dato);
+		
+		System.out.println("\nVector de primos hasta: " + dato);
 		for (int i = 0; i < vector.length; i++) {
 			if (i % 10 == 0)
 				System.out.println();
-			System.out.println(vector[i] + "/t");
+			System.out.println(vector[i] + "\t");
 		}
 	}
 }
